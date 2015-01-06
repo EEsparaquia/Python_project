06e4e96b256bb8b3f763944994e7c36e5769bba3
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 01:10:51 -0600

    ELSE estatement

---
+++ script8.py
@@ -0,0 +1,20 @@
+#! Python3
+
+# Programming tutorial: Else Statement
+
+x = 5
+y = 8
+
+if x > y:
+	print('"x" is greater than "y"\n')
+else:
+	print('"x" is less than "y"\n')
+
+# More than one if
+
+if x > y:
+	print('"x" is greater than "y"\n')
+if x < 55:
+	print('"x" is greater than 55')
+else:
+	print('"x" is less than "y"\n')
