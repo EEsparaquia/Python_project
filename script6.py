94bd08800b65aa99600d7e9526e6185d8cbc3afa
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 00:58:32 -0600

    For loop

---
+++ script6.py
@@ -0,0 +1,20 @@
+#! Python3
+
+# Programming tutorial: For loop
+
+#List of variables
+exampleList = [1,2,3,4,5,6,7,80,54,32,46,31,0]
+
+'''
+	Create a new variable eachNumber, 
+	and exampleList
+	asign his values to eachNumber:
+'''
+
+for eachNumber in exampleList:
+	print(eachNumber)
+	print('Identation really matters')
+
+#Abother function 
+for x in range(1,11):
+	print(x)
