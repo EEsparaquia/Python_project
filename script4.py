a52bc1de1988578426265e1b203dc30fa09a2c21
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 00:43:17 -0600

    Varibles

---
+++ script4.py
@@ -0,0 +1,19 @@
+#! Python3
+
+# Programming tutorial: Variables
+
+#Variables handle some value
+exampleVar = 55
+print(exampleVar)
+
+#Variable can do simple operations
+exampleVar2 = 55+33
+print(exampleVar2)
+
+#A variable can handle a function
+exampleVar3 = print('Jamon')
+
+#Pack Variables
+x,y = (3,5)
+print(x)
+print(y)
