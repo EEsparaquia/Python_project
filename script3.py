bba01fec93ff9f8e75e6f018e956c2c6354fa629
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 00:36:43 -0600

    Basic Math

---
+++ script3.py
@@ -0,0 +1,16 @@
+#! Python3
+
+# Programming tutorial: Math
+# + - * /
+print('1+3')
+print(1+3,'\n')
+print('3-1')
+print(3-1,'\n')
+print('4*4')
+print(4*4,'\n')
+print('5/34')
+print(5/34,'\n')
+print('5%34')
+print(5%34,'\n')
+print('4**4')
+print(4**4,'\n')
