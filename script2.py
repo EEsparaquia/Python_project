fe19a8b1dcc136fd4d7fd0154d0388bb4cc243db
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 00:23:06 -0600

    print function and Strings

---
+++ script2.py
@@ -0,0 +1,20 @@
+#! Python3 
+
+# Print functions and String
+
+print('This is an example of print function')
+#Everything in single quots is an String
+print("\n")
+print("This is an example of 'Single quots' " )
+print("\n")
+print('We\'re going to store')
+print("\n")
+print('Hi'+'There')
+#Plus sign concatenate both strings
+print('Hi','There')
+#Comma add an space
+print("\n")
+print('Hi',5)
+print('Hi '+str(5))
+print("\n")
+print(float('8.5')+5)
