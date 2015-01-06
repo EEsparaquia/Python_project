9c7f6ce05e3342fedefdb5732933be85e8b7a526
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-06 13:14:35 -0600

    Elif statement

---
+++ script9.py
@@ -0,0 +1,16 @@
+#! Python3
+
+# Programming tutorial: if Elif Else Statements
+
+x = 4
+y = 10
+z = 22
+
+if x > y:
+	print('"X" is greater than "y"')
+elif x < z:  
+	print('"X" is less than "y"')
+elif 5 > 2:  
+	print('"5" is greater than "2"')
+else:
+	print('if and elfi(s) never ran')
