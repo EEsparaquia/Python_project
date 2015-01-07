54ca685c71a27e68d41437e8b5620c0289bf8669
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-07 14:13:39 -0600

    Tuple vs list

---
+++ script22.py
@@ -0,0 +1,23 @@
+#! Python3
+
+# Programming tutorial: 
+# Tuple vs List
+
+# Tuple Example
+x = 6,5,4,3
+x = (6,5,4,3)
+print(x[1])
+
+# Python List
+y = [5,4,3,2]
+print(y[1])
+
+
+# Example of use from a Tuple
+# If you dont need to change 
+# the values
+def example():
+	return 12,6
+
+x,y = example()
+
