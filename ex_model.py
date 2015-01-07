779b58ea06df9e3e653d3ff33b127f8b53f87a76
Ernesto Esparaquia <esparaquia@outlook.com>
2015-01-07 14:05:48 -0600

    Creation modules and call it from another script

---
+++ ex_model.py
@@ -0,0 +1,7 @@
+#! Python3
+
+# Programming tutorial: 
+# Module creation
+
+def ex(data):
+	print(data)
---
+++ script21.py
@@ -0,0 +1,9 @@
+#! Python3
+
+# Programming tutorial: 
+# Module import Syntax
+# local/lib/pyPackage
+
+import ex_model
+
+ex_model.ex('test')
